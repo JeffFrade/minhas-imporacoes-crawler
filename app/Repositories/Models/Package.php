98ffe8bf3db470cc8b8b4617548490918e49aa46
Jefferson Frade <jefferrson.frade@gmail.com>
2022-01-10 11:39:15 -0300

    Removed packages and created Table, Model and Repository

---
+++ app/Repositories/Models/Package.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repositories\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Package extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'tracking_number',
+        'status'
+    ];
+}
